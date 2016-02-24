449a9886a5865843e997b2ad9e39fc53acdda491
Joel Verhagen <jver@microsoft.com>
2016-02-23 19:06:37 -0800

    Merge WebApiConfig into Startup

--- ConnectorRide.Web/Startup.cs
+++ ConnectorRide.Web/Startup.cs
@@ -1,8 +1,9 @@
-﻿using System;
+﻿using System.Net.Http.Formatting;
 using System.Web.Http;
-using System.Web.Http.Routing;
+using System.Web.Http.ExceptionHandling;
 using Knapcode.ConnectorRide.Web;
 using Microsoft.Owin;
+using Newtonsoft.Json;
 using Owin;
 
 [assembly: OwinStartup(typeof (Startup))]
@@ -15,9 +16,26 @@ namespace Knapcode.ConnectorRide.Web
         {
             var httpConfiguration = new HttpConfiguration();
 
-            WebApiConfig.Register(httpConfiguration);
+            Register(httpConfiguration);
 
             app.UseWebApi(httpConfiguration);
+        }
+
+        private static void Register(HttpConfiguration config)
+        {
+            config.Formatters.Clear();
+            config.Formatters.Add(new JsonMediaTypeFormatter
+            {
+                UseDataContractJsonSerializer = false,
+                SerializerSettings = new JsonSerializerSettings()
+            });
+
+            config.Services.Replace(typeof(IExceptionHandler), new ExceptionHandler());
+
+            config.Routes.MapHttpRoute(
+                "CommandsUpdate",
+                "commands/update",
+                new { controller = "Commands", action = "UpdateSchedulesAsync" });
         }
     }
 }
